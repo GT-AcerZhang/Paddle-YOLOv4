8769e7324418974bcc4fc9d4aa52fe3c4c42cead
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2020-07-16 21:01:34 +0800

    lr

--- config.py
+++ config.py
@@ -130,7 +130,7 @@ class YOLOv3_Config_1(object):
 
         # 模式。 0-从头训练，1-读取之前的模型继续训练（model_path可以是'yolov4.h5'、'./weights/step00001000.h5'这些。）
         self.pattern = 1
-        self.lr = 0.0001
+        self.lr = 0.00001
         self.batch_size = 8
         # 如果self.pattern = 1，需要指定self.model_path表示从哪个模型读取权重继续训练。
         self.model_path = 'yolov3_r50vd_dcn_obj365_dropblock_iouloss'
